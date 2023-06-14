1694e68b75502653ef4e5537a144a3b98be74e4f
Chandrashekhar-Bhavsar <chandrashekhar.bhavsar.kh.dbda@gmail.com>
2023-06-14 14:13:32 +0530

    comment module

--- pmt.py
+++ pmt.py
@@ -633,5 +633,193 @@ def get_defect_count():
 
 
 
+    ######################################################################################################
+
+
+def add_comment():
+    """
+    API endpoint for adding a comment to a project.
+
+    Returns:
+        If successful, returns the result of the 'project_commentadd' function.
+        If any errors occur during execution, returns a JSON response with an error message and an appropriate status code.
+
+    Raises:
+        KeyError: If any of the required fields ('project_id', 'user_id', 'description') are missing in the request data.
+        mysql.connector.Error: If there is an error related to the MySQL database.
+        Exception: If any other unexpected exception occurs.
+
+    Usage:
+        - Send a POST request to the 'add_projectcomment' endpoint.
+        - The request data must be in JSON format and include the following fields:
+            - 'project_id' (integer): The ID of the project to add the comment to.
+            - 'user_id' (integer): The ID of the user adding the comment.
+            - 'description' (string): The content of the comment to be added.
+"""
+
+
+    try:
+        now = datetime.now()
+        dt_string = str(now.strftime("%d/%m/%Y %H:%M:%S"))
+        logging.debug(dt_string + " Inside add_project_comment api....")
+        data = request.get_json()
+        print(data)
+        logging.debug(dt_string + " Accepting values for add project comment.....")
+
+        if "id" not in data:
+            return jsonify({"error": "Missing 'project_id' in request data"}), 400
+        if "user_id" not in data:
+            return jsonify({"error": "Missing 'user_id' in request data"}), 400
+        if "description" not in data:
+            return jsonify({"error": "Missing 'description' in request data"}), 400
+        
+        id=data['id']
+        
+        user_id =data["user_id"]
+        
+        description=data["description"]
+
+        if(type(user_id) is not int):
+            return jsonify({"error":"user_id must be integer"}),400
+        if(type(id) is not int):
+            return jsonify({"error":"id must be integer"}),400
+        
+        logging.debug(dt_string + ' calling project_commentadd function.....')
+
+        return commentadd(id,description,user_id)
+
+    except KeyError as e:
+        # Handle missing key in the request data
+        #print("Missing key in request data: " + str(e))
+        
+        return jsonify({"error": " Missing key " + str(e)}), 400
+
+    except mysql.connector.Error as err:
+        # Handle MySQL database-related errors
+        print("Database error: " + str(err))
+        
+        return jsonify({"error": "Database error: " + str(err)}), 500
+
+    except Exception as e:
+        # Handle any other unexpected exceptions
+        print("An error occurred: " + str(e))
+        return jsonify({"error": "An error occurred: " + str(e)}), 500
     
+#########################################################################################################
+
+
+def display_comments():
+    """
+    API endpoint for displaying comments for a specific issue.
+
+    Returns:
+        If successful, returns the result of the 'displaycomments_issuewise' function.
+        If any errors occur during execution, returns a JSON response with an error message and an appropriate status code.
+
+    Raises:
+        KeyError: If the required field 'issue_id' is missing in the request data.
+        mysql.connector.Error: If there is an error related to the MySQL database.
+        Exception: If any other unexpected exception occurs.
+
+    Usage:
+        - Send a POST request to the 'display_issuewisecomments' endpoint.
+        - The request data must be in JSON format and include the following field:
+            - 'issue_id' (integer): The ID of the issue for which to retrieve the comments.
+    """
+    
+    try:
+        now = datetime.now()
+        dt_string = str(now.strftime("%d/%m/%Y %H:%M:%S"))
+        logging.debug(dt_string + " Inside display_issuewise_comments....")
+        data = request.get_json()
+        logging.debug(dt_string + ' Accepting issue_id to display issue wise comments.....')
+        #project_id= data["project_id"]
+
+        if "id" not in data:
+            return jsonify({"error": "Missing 'id' in request data"}), 400
+        id=data["id"]
+       
+        if(type(id) is not int):
+            return jsonify({"error":"id must be integer"}),400
+        
+        
+        logging.debug(dt_string + " calling displaycomments function......")
+
+        return displaycomments(id)
+
+    except KeyError as e:
+        # Handle missing key in the request data
+        #print("Missing key in request data: " + str(e))
+        return jsonify({"error": str(e)}), 400
+
+    except mysql.connector.Error as err:
+        # Handle MySQL database-related errors
+        print("Database error: " + str(err))
+        return jsonify({"error": "Database error: " + str(err)}), 500
+
+    except Exception as e:
+        # Handle any other unexpected exceptions
+        print("An error occurred: " + str(e))
+        return jsonify({"error": "An error occurred: " + str(e)}), 500
+    
+
+
+    ######################################################################################################
+    ###############################################################################################################
+
+
+
+def delete_comment():
+    """
+    API endpoint for deleting a comment.
+
+    Returns:
+        If successful, returns the result of the 'delete_comments' function.
+        If any errors occur during execution, returns a JSON response with an error message and an appropriate status code.
+
+    Raises:
+        KeyError: If the required field 'comment_id' is missing in the request data.
+        mysql.connector.Error: If there is an error related to the MySQL database.
+        Exception: If any other unexpected exception occurs.
+
+    Usage:
+        - Send a POST request to the 'delete_comment' endpoint.
+        - The request data must be in JSON format and include the following field:
+            - 'comment_id' (integer): The ID of the comment to be deleted.
+    """
+    try:
+        now = datetime.now()
+        dt_string = str(now.strftime("%d/%m/%Y %H:%M:%S"))
+        logging.debug(dt_string + " Inside update_issuewise_comments api....")
+        data = request.get_json()
+
+        if "comment_id" not in data:
+            return jsonify({"error": "Missing 'comment_id' in request data"}), 400
+    
+
+        logging.debug(dt_string + " Accepting values to update ")
+
+        
+        comment_id=data["comment_id"]
+     
+        if(type(comment_id) is not int):
+            return jsonify({"error":"comment_id must be integer"}),400
+
+        logging.debug(dt_string + " Calling updateissuewise_comments function to update the database.....")
+        return delete_comments(comment_id)
+        
+
+    except KeyError as e:
+        # Handle missing key in the request data
+        return jsonify({"error":  + str(e)}), 400
+
+    except mysql.connector.Error as err:
+        # Handle MySQL database-related errors
+        print("Database error: " + str(err))
+        return jsonify({"error": "Database error: " + str(err)}), 500
+
+    except Exception as e:
+        # Handle any other unexpected exceptions
+        print("An error occurred: " + str(e))
+        return jsonify({"error": "An error occurred: " + str(e)}), 500
 
